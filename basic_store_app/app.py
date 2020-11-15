f4ce4f86ec2bf23c79c0a6dfc886d410ae19db1b
Andy Saul <andycsaul@gmail.com>
2020-11-15 14:23:42 +0000

    Implement create_item_in_store function

--- basic_store_app/app.py
+++ basic_store_app/app.py
@@ -12,7 +12,7 @@ stores = [
 ]
 
 
-# POST /store/ data: {name:}
+# POST /store data: {name:}
 @app.route('/store', methods=['POST'])
 def create_store():
     request_data = request.get_json()
@@ -24,7 +24,7 @@ def create_store():
     return jsonify(new_store)
 
 
-# GET /store/<string:name>/
+# GET /store/<string:name>
 @app.route('/store/<string:name>')
 def get_store(name):
     for store in stores:
@@ -39,13 +39,21 @@ def get_stores():
     return jsonify({'stores': stores})
 
 
-# POST /store/<string:name>/ data: {name:, price}
+# POST /store/<string:name> data: {name:, price}
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
-    pass
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(new_item)
+    return jsonify({'message': 'store not found'})
 
-
-# GET /store/<string:name>/item/
+# GET /store/<string:name>/item
 @app.route('/store/<string:name>/item')
 def get_item_in_store(name):
     for store in stores:
