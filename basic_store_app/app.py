8b1fc32bf7fc2b7a8af351868b57e88e660dc5e8
Andy Saul <andycsaul@gmail.com>
2020-11-15 12:18:10 +0000

    Implement create_store function

--- basic_store_app/app.py
+++ basic_store_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -12,31 +12,37 @@ stores = [
 ]
 
 
-# POST /store data: {name:}
+# POST /store/ data: {name:}
 @app.route('/store', methods=['POST'])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
 
 
-# GET /store/<string:name>
+# GET /store/<string:name>/
 @app.route('/store/<string:name>')
 def get_store(name):
     pass
 
 
 # GET /store/
-@app.route('/store/')
+@app.route('/store')
 def get_stores():
     return jsonify({'stores': stores})
 
 
-# POST /store/<string:name> data: {name:, price}
+# POST /store/<string:name>/ data: {name:, price}
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
     pass
 
 
-# GET /store/<string:name>/item
+# GET /store/<string:name>/item/
 @app.route('/store/<string:name>/item')
 def get_item_in_store(name):
     pass
