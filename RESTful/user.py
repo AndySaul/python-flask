eff2386d30372203334bd522b1d66816cf6e977d
Andy Saul <andycsaul@gmail.com>
2020-11-23 20:37:57 +0000

    Use with statement for Database class
    Use with statement for Database class
    
    Use with statement for Database class

--- RESTful/user.py
+++ RESTful/user.py
@@ -2,6 +2,25 @@ import sqlite3
 from flask_restful import Resource, reqparse
 
 
+class Database:
+    def __init__(self, name='data.db'):
+        self._name = name
+        self._connection = None
+        self._cursor = None
+
+    def __enter__(self):
+        self._connection = sqlite3.connect(self._name)
+        self._cursor = self._connection.cursor()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._connection.commit()
+        self._connection.close()
+
+    def execute(self, *args):
+        return self._cursor.execute(*args)
+
+
 class User:
     def __init__(self, _id, username: str, password: str):
         self.id = _id
@@ -10,44 +29,29 @@ class User:
 
     @classmethod
     def find_by_username(cls, username):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query, (username,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-        connection.close()
-        return user
+        with Database() as db:
+            query = "SELECT * FROM users WHERE username=?"
+            result = db.execute(query, (username,))
+            row = result.fetchone()
+            if row:
+                return cls(*row)
+        return None
 
     @classmethod
     def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query, (_id,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-        connection.close()
-        return user
+        with Database() as db:
+            query = "SELECT * FROM users WHERE id=?"
+            result = db.execute(query, (_id,))
+            row = result.fetchone()
+            if row:
+                return cls(*row)
+        return None
 
     @classmethod
     def create_table(cls):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
-        cursor.execute(create_table)
-
-        connection.commit()
-        connection.close()
+        with Database() as db:
+            create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
+            db.execute(create_table)
 
 
 class RegisterUser(Resource):
@@ -56,21 +60,13 @@ class RegisterUser(Resource):
         User.create_table()
 
         params = self._parse_params()
-
-        user = User.find_by_username(params['username'])
-        if user:
+        if User.find_by_username(params['username']):
             return {'message': "User already registered"}, 400
 
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO users VALUES (NULL, ?, ?)"
-        args = (params['username'], params['password'])
-        cursor.execute(query, args)
-
-        connection.commit()
-        connection.close()
-
+        with Database() as db:
+            query = "INSERT INTO users VALUES (NULL, ?, ?)"
+            args = (params['username'], params['password'])
+            db.execute(query, args)
         return {'message': "User created successfully"}, 201
 
     @staticmethod
