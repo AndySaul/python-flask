af0f7b96979532234af320a229901ac8cc0c7108
Andy Saul <andycsaul@gmail.com>
2020-12-08 20:43:04 +0000

    Add an optional JWT authentication

--- resources/item.py
+++ resources/item.py
@@ -1,16 +1,20 @@
 # Copyright (c) Andy Saul 2020
 
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import get_jwt_claims, jwt_required
+from flask_jwt_extended import jwt_required, jwt_optional, get_jwt_claims, get_jwt_identity
 
 from models.item import ItemModel
 
 
 class Items(Resource):
 
-    @jwt_required
+    @jwt_optional
     def get(self):
-        return {'items': [item.json() for item in ItemModel.find_all()]}
+        user_id = get_jwt_identity()
+        items = [item.json() for item in ItemModel.find_all()]
+        if user_id:
+            return {'items': items}, 200
+        return {'items': [item['name'] for item in items], 'message': 'More data available if you log in'}, 200
 
 
 class Item(Resource):
